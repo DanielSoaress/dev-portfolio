092f06f5f336f705dc84e474fc68ada2a982bbaa
Daniel Soares <daniel.soares@wapstore.com.br>
2021-06-03 21:00:33 -0300

    Update Contact

--- src/view/Card.vue
+++ src/view/Card.vue
@@ -1,85 +1,23 @@
 <template>
     <b-container class="card-presentation-container" >
         <Particles />
-        <div class="card-presentation p-4">
-            <b-img class="mb-4 mx-auto" width="150" height="150" :src="require('../assets/perfil.jpg')" rounded="circle" alt="Perfil image"></b-img>
-            <div class="text">
-                <b>Daniel Soares</b> <br> <span class="cargo">FullStack Developer</span>
-            </div>
-            <b-row class="mt-3">
-                <b-col cols="6">
-                    <b-button href="https://github.com/DanielSoaress"  class="w-100 d-flex align-items-center justify-content-center">
-                        <b-img  class="mr-1" width="20" height="20" :src="require('../assets/github-white.png')"></b-img>
-                        <span>Github</span></b-button>
-                </b-col>
-                <b-col cols="6">
-                    <b-button href="https://www.linkedin.com/in/daniel-soaress/" class="w-100 d-flex align-items-center justify-content-center">
-                        <b-img  class="mr-1 linkedin" width="16" height="16" :src="require('../assets/icons/linkedin.png')"></b-img>
-                        <span >Linkedin</span>
-                    </b-button>
-                </b-col>
-            </b-row>
-            <b-row class="mt-2">
-                <b-col cols="6">
-                    <b-button href="https://danielsoares.dev.br/" class="w-100 d-flex align-items-center justify-content-center">
-                        <b-img  class="mr-1" width="20" height="20" :src="require('../assets/icons/global.png')"></b-img>
-                        <span>Portfólio</span></b-button>
-                </b-col>
-                <b-col cols="6">
-                    <b-button href="https://www.instagram.com/lost_dsf/" class="w-100 d-flex align-items-center justify-content-center">
-                        <b-img  class="mr-1" width="20" height="20" :src="require('../assets/icons/instagram.png')"></b-img>
-                        <span>Instagram</span></b-button>
-                </b-col>
-            </b-row>
-            <p class="email">danielsoaresf@hotmail.com</p>
-        </div>
+        <Card />
     </b-container>
 </template>
 
 <script>
 import Particles from "@/components/Particles"
+import Card from "@/components/CardPresentation"
 
 export default {
     components: {
-        Particles
+        Particles,
+        Card
     }
 }
 </script>
 
 <style>
-    .card-presentation-container {
-        height: 100vh;
-        width: 100vw;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-    .card-presentation  {
-        max-width: 300px;
-        height: auto;
-        margin: 0 auto;
-        background: rgba( 255, 255, 255, 0.1);
-        border-radius: 10px;
-        border: 1px solid rgba( 255, 255, 255, 0.18 );
-    }
-
-    .card-presentation .linkedin {
-        position: relative;
-        top: -2.5px;
-    }
-
-    .card-presentation .text {
-        font-size: 1.5rem;
-        line-height: 1.4rem;
-    }
-    .card-presentation .cargo {
-        font-size: 1.125rem;
-    }
-
-    .card-presentation .email {
-        font-size: 0.875rem;
-        margin-bottom: 0px;
-        margin-top: 15px;
-    }
+  
 
 </style>
--- src/view/Contact.vue
+++ src/view/Contact.vue
@@ -1,24 +1,16 @@
 <template>
     <b-container>
-       <b-row class="mb-5">
-         <b-row class="d-lg-none hidden-custom align-center-custom align-center-custom w-100">
-              <b-img width="300" height="300" :src="require('../assets/MobileMarketing-bro.png')" fluid alt="Responsive image"></b-img>
+       <b-row class="my-5">
+         <b-row class="d-lg-none hidden-custom mx-auto mb-5 align-center-custom align-center-custom w-100">
+               <CardPresentation />
         </b-row>
         <b-col class="d-none d-lg-block d-print-block">
-          <b-img  width="500" height="500" :src="require('../assets/MobileMarketing-bro.png')" fluid alt="Responsive image"></b-img>
+          <CardPresentation  class="mx-auto" />
         </b-col>
         <b-col class="d-flex align-items-center">
           <div>
           <p class="text-justify">Tem dúvidas de como posso ajudar a sua marca? Com certeza, terei respostas para lhe auxiliar, por isso não perca tempo e entra em contato comigo agora.</p>
           <p class="text-justify">ou quer apenas trocar um papo, só me chamar, ficarei feliz em repondê lo.</p>
-          <p class="text-justify">Email: <b>danielsoaresf@hotmail.com</b></p>
-          <a class="mr-3 btn-contact" href="https://github.com/DanielSoaress" >            
-            <b-img width="50" height="50" :src="require('../assets/github.png')"></b-img>
-          </a>
-          <a class="mr-3 btn-contact" href="https://www.linkedin.com/in/daniel-soares-26b718145/" >            
-            <b-img width="50" height="50" :src="require('../assets/linkedin.png')"></b-img>
-          </a>
-          <Button class='mt-3' to="card" txt="SABER MAIS"/>
           </div>
         </b-col>
       </b-row>  
@@ -26,11 +18,11 @@
 </template>
 
 <script>
-import Button from "@/components/Button"
+import CardPresentation from "@/components/CardPresentation"
 
 export default {
   components: {
-    Button
+    CardPresentation
   }
 }
 </script>
