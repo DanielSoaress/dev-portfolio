cfd1de7356f3f32f9495c0c7e5942ee2e34c6b47
Daniel Soares <daniel.soares@wapstore.com.br>
2021-06-07 08:41:55 -0300

    pagespeed animate.css

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router/router.js'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import Particles from "particles.vue";
 import ScrollAnimation from "./directive/scrollanimation"
+import 'animate.css';
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
